4ade8870c2bfc238f29a5e2188e2e0844d0804a5
david6686 <alucard6686@gmail.com>
2018-03-17 00:31:21 +0800

    測試中文latex

---
+++ auto/document.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook
+ "document"
+ (lambda ()
+   (setq TeX-command-extra-options
+         "-shell-escape")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"))
+ :latex)
+
