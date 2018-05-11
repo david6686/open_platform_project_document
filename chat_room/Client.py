142c6a244eda292734fe996a1a9238910a27acea
黃宇睿 <alucard6686@gmail.com>
2018-05-11 13:57:54 +0800

    增加註解

--- chat_room/Client.py
+++ chat_room/Client.py
@@ -6,13 +6,13 @@ class Client:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock = sock
         self.sock.connect((host, port))
-        self.sock.send(b'1')
+        self.sock.send(b'1')   #byte方式 傳送  送key 假設是1
 
     def sendThreadFunc(self):
         while True:
             try:
-                myword = input()
-                self.sock.send(myword.encode())
+                myword = input()     #不斷的等待接收使用者的輸入
+                self.sock.send(myword.encode())    # 一樣encode起來送出去
             except ConnectionAbortedError:
                 print('Server closed this connection!')
             except ConnectionResetError:
@@ -21,7 +21,7 @@ class Client:
     def recvThreadFunc(self):
         while True:
             try:
-                otherword = self.sock.recv(1024) # socket.recv(recv_size)
+                otherword = self.sock.recv(1024) # socket.recv(recv_size)    ＃去接收  1024是接收的長度
                 print(otherword.decode())
             except ConnectionAbortedError:
                 print('Server closed this connection!')
@@ -30,12 +30,14 @@ class Client:
                 print('Server is closed!')
 
 def main():
-    c = Client('localhost', 5550)
+    c = Client('localhost', 5550)    #new Client    addr,port number  要看誰當server喔
+    #  切兩個thread  兩個同時跑  不斷的收與送
     th1 = threading.Thread(target=c.sendThreadFunc)
     th2 = threading.Thread(target=c.recvThreadFunc)
     threads = [th1, th2]
+    # 兩個thread一起初始化
     for t in threads:
-        t.setDaemon(True)
+        t.setDaemon(True)   #父執行緒不用等子執行緒
         t.start()
     t.join()
 
--- chat_room/Server.py
+++ chat_room/Server.py
@@ -1,4 +1,7 @@
 # -*- encoding: utf-8 -*-
+
+#上面那個是encode指定utf-8，這樣子才可以有中文傳輸的功能
+
 import socket
 import threading
 from time import gmtime, strftime
@@ -6,32 +9,38 @@ from time import gmtime, strftime
 
 class Server:
     def __init__(self, host, port):
+        # new一個socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock = sock
-        self.sock.bind((host, port))
-        self.sock.listen(5)
-        print('Server', socket.gethostbyname(host), 'listening ...')
+        self.sock.bind((host, port))  # 用bind方式  不斷監聽
+        self.sock.listen(5)    #listen   可以包含五個連線？
+        print('Server', socket.gethostbyname(host), 'listening ...')    #做完上面，會說在哪個ip做listen
         self.mylist = list()
 
+    #確認註冊過的使用者，看要不要接收他
     def checkConnection(self):
         connection, addr = self.sock.accept()
+        #現在有新的連線進來
         print('Accept a new connection', connection.getsockname(), connection.fileno())
 
         try:
+            #第一個進來的 送的訊息是不是b'1'  是1就接受  並開一個thread給他
             buf = connection.recv(1024).decode()
             if buf == '1':
                 # start a thread for new connection
                 mythread = threading.Thread(target=self.subThreadIn, args=(connection, connection.fileno()))
                 mythread.setDaemon(True)
                 mythread.start()
-
+            # 反之不合法
             else:
-                connection.send(b'please go out!')
+                connection.send(b'please go out!daze~')
                 connection.close()
         except:
             pass
 
     # send whatToSay to every except people in exceptNum
+    #把connect id 記住，會加進mylist內，就可以做出線上人數多少人
+    #傳送訊息的function也就是從list內把除了自己以外的所有人拿出來並傳送訊息（恩...所以送給自己知道怎麼做了吧）
     def tellOthers(self, exceptNum, whatToSay):
         for c in self.mylist:
             if c.fileno() != exceptNum:
@@ -41,8 +50,10 @@ class Server:
                     pass
 
     def subThreadIn(self, myconnection, connNumber):
+        # 把使用者connect id加進list
         self.mylist.append(myconnection)
         while True:
+            #不斷接收新的訊息，並送出訊息
             try:
                 recvedMsg = myconnection.recv(1024).decode()
                 if recvedMsg:
@@ -51,6 +62,7 @@ class Server:
                     pass
 
             except (OSError, ConnectionResetError):
+                #下線就要從list移除並關掉connect
                 try:
                     self.mylist.remove(myconnection)
                 except:
@@ -62,6 +74,7 @@ class Server:
 
 def main():
     s = Server('localhost', 5550)
+    #不斷的checkconnect
     while True:
         s.checkConnection()
 
